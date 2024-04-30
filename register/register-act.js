4af547111f5cff72a5e73b382b2a4aefe42388e0
wixucys <alisshurman@gmail.com>
2024-04-30 19:02:06 +0300

    папка страницы регистрации

---
+++ register/register-act.js
@@ -0,0 +1,15 @@
+function show_hide_password(target) {
+  var input = document.getElementById("password");
+  var iconShow = document.querySelector(".icon-show");
+  var iconHide = document.querySelector(".icon-hide");
+  if (input.getAttribute("type") == "password") {
+    input.setAttribute("type", "text");
+    iconShow.style.setProperty("display", "none");
+    iconHide.style.removeProperty("display");
+  } else {
+    input.setAttribute("type", "password");
+    iconHide.style.setProperty("display", "none");
+    iconShow.style.removeProperty("display");
+  }
+  return false;
+}
