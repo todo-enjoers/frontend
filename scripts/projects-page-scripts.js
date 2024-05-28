e221e0030a8a2b85d7821c4c7c1c319b0b66f929
wixucys <alisshurman@gmail.com>
2024-05-28 14:11:52 +0300

    Верстка страницы с досками

--- scripts/projects-page-scripts.js
+++ scripts/projects-page-scripts.js
@@ -3,18 +3,18 @@ function openNav() {
   document.getElementById("side-bar").style.width = "250px";
   document.querySelector(".app").style.filter = "brightness(40%)";
   document.querySelector(".app").style.transition = "filter 0.8s";
-  document.getElementById("addProject").setAttribute("disabled", "true");
+  document.getElementById("addBoard").setAttribute("disabled", "true");
 }
 
 function closeNav() {
   document.getElementById("side-bar").style.width = "0";
   document.querySelector(".app").style.filter = "";
   document.querySelector(".app").style.transition = "filter 0.8s";
-  document.getElementById("addProject").removeAttribute("disabled", "true");
+  document.getElementById("addBoard").removeAttribute("disabled", "true");
 }
 
 // Открытие/закрытие модального окна с созданием проекта
-const button = document.getElementById("addProject");
+const button = document.getElementById("addBoard");
 const closeBtn = document.querySelector(".modal-close-button");
 
 button.addEventListener("click", () => {
@@ -25,33 +25,58 @@ closeBtn.addEventListener("click", () => {
   document.querySelector(".modal").classList.remove("modal-active");
 });
 
-// Создание проекта и добавление его в список
-function createProject() {
-  const createBtn = document.getElementById("createProject");
-  const projects = document.getElementById("menu");
-  var textarea = document.querySelector(".project-name-text");
+// Создание доски и добавление на страницу
+const btn = document.getElementById("createBoard");
+var textarea = document.getElementById("boardName").value;
 
-  let value;
+function addBoards() {
+  const boards = document.querySelector(".boards");
+  const board = document.createElement("div");
+  board.classList.add("boards-item");
+  board.innerHTML = `
+  <span class="title">${textarea}</span>
+  <span class="del-board">&#10006</span>
 
-  textarea.addEventListener("input", (e) => (value = e.target.value));
+  <div class="list"></div>`;
+  boards.append(board);
 
-  createBtn.addEventListener("click", () => {
-    const project = document.createElement("span");
-    project.classList.add("project-name");
-    project.textContent = textarea.value;
-    projects.appendChild(project);
-    textarea.value = "";
-    value = "";
-
-    const btn = document.createElement("button");
-    btn.classList.add("del-project");
-    btn.textContent = "Удалить";
-    projects.appendChild(btn);
-
-    btn.addEventListener("click", () => {
-      project.remove();
-      btn.remove();
-    });
-  });
+  delBoard();
 }
-createProject();
+
+btn.addEventListener("click", () => {
+  addBoards();
+  textarea = "";
+});
+
+function delBoard() {
+  var delboards = document.querySelectorAll(".del-board");
+  var boards = document.querySelectorAll(".boards-item");
+
+  for (i = 0; i < boards.length; i++) {
+    const board = boards[i];
+    const delboard = delboards[i];
+
+    delboard.addEventListener("click", () => {
+      board.remove();
+    });
+  }
+}
+
+// function addTask() {
+//   // const cancelBtn = document.getElementById("close-btn");
+//   var lists = document.querySelectorAll(".list");
+
+//   var name = document.querySelector(".task-name").value;
+//   var form = document.querySelector(".task-description").value;
+
+//   const list = document.createElement("div");
+//   list.classList.add("list__item");
+//   list.innerHTML = `<span data-bs-target="#taskModal">${name}</span>`;
+//   lists.appendChild(list);
+// }
+
+// btn.addEventListener("click", () => {
+//   addTask();
+//   var textarea = document.querySelector(".task-name");
+//   textarea.value = "";
+// });
